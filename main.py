d3d8a3d7e501807af63d9be94dcbd9334a9fd67f
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-29 22:15:51 +0300

    bump

--- main.py
+++ main.py
@@ -10,37 +10,45 @@ load_dotenv()
 SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
-new_playlist_name = "Liked Songs by kiwi"
 
 scope = "user-library-read playlist-modify-private"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 
-current_user_id = sp.current_user()["id"]
-# Create a new private playlist with the provided name
-new_playlist = sp.user_playlist_create(user=current_user_id, name=new_playlist_name, public=False)
-# Get the user's saved tracks
-saved_tracks = []
-offset = 0
-limit = 50  # Maximum number of tracks per API request
-while True:
-    results = sp.current_user_saved_tracks(limit=limit, offset=offset)
-    saved_tracks.extend(results["items"])
-    if len(results["items"]) < limit:
-        # All saved tracks fetched
-        break
-    offset += limit
-# Extract track URIs from the saved tracks
-track_uris = [track["track"]["uri"] for track in saved_tracks]
 
-for i in range(0, len(track_uris), 50):
-        batch = track_uris[i:i + 50]
-        sp.playlist_add_items(playlist_id=new_playlist["id"], items=batch)
-        if i + 50 < len(track_uris):
-            # Introduce a delay between consecutive calls to avoid rate limit
-            time.sleep(1)
-        
+def save_shortest_tracks_to_playlist(ource_playlist_name, new_playlist_name):
+    # Initialize Spotipy client with OAuth2 authentication
+    scope = "user-library-read playlist-modify-private"
 
-print(f"All saved tracks have been added to the playlist '{new_playlist_name}'.")
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    # Get the current user's ID
+    current_user_id = sp.current_user()["id"]
+
+    # Get the source playlist ID by searching for the playlist name
+    source_playlist = None
+    playlists = sp.current_user_playlists()
+    for playlist in playlists["items"]:
+        if playlist["name"] == source_playlist_name:
+            source_playlist = playlist
+            break
+
+    if not source_playlist:
+        print(f"Playlist '{source_playlist_name}' not found.")
+        return
+
+    # Create a new private playlist with the provided name
+    new_playlist = sp.user_playlist_create(user=current_user_id, name=new_playlist_name, public=False)
+
+    sp.playlist_add_items(playlist_id=new_playlist["id"], items=shortest_track_uris)
+
+    print(f"The 20 shortest tracks from '{source_playlist_name}' have been added to the playlist '{new_playlist_name}'.")
+
 # Example usage
+if __name__ == "__main__":
+    source_playlist_name = "My Shazam Tracks"
+    new_playlist_name = "test1"
+
+
+    save_shortest_tracks_to_playlist(source_playlist_name, new_playlist_name)
