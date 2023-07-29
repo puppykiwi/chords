39bfa9a667aa39f3c2376523e3e36210c6648189
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-29 23:11:29 +0300

    get_playlist_id

--- main.py
+++ main.py
@@ -11,44 +11,68 @@ SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 
-scope = "user-library-read playlist-modify-private"
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
-
-
-def save_shortest_tracks_to_playlist(ource_playlist_name, new_playlist_name):
-    # Initialize Spotipy client with OAuth2 authentication
+def get_playlist_id(playlist_name)
     scope = "user-library-read playlist-modify-private"
-
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-    # Get the current user's ID
+
     current_user_id = sp.current_user()["id"]
 
-    # Get the source playlist ID by searching for the playlist name
-    source_playlist = None
-    playlists = sp.current_user_playlists()
-    for playlist in playlists["items"]:
-        if playlist["name"] == source_playlist_name:
-            source_playlist = playlist
-            break
+    playlists = sp.user_playlists(user=current_user_id)
+    if playlists["items"]:
+        for playlist in playlists["items"]:
+            if playlist["name"] == playlist_name:
+                print("Playlist ID: ", playlist["id"]) # debug
+                return playlist["id"]
 
-    if not source_playlist:
-        print(f"Playlist '{source_playlist_name}' not found.")
-        return
 
-    # Create a new private playlist with the provided name
+def save_shortest_songs_to_playlist(source_playlist_id, new_playlist_name):
+    scope = "user-library-read playlist-modify-private"
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+
+    current_user_id = sp.current_user()["id"]
+
     new_playlist = sp.user_playlist_create(user=current_user_id, name=new_playlist_name, public=False)
 
-    sp.playlist_add_items(playlist_id=new_playlist["id"], items=shortest_track_uris)
+    # Get all tracks from the source playlist using pagination
+    tracks = []
+    offset = 0
+    limit = 100  # Maximum number of tracks per API request
 
-    print(f"The 20 shortest tracks from '{source_playlist_name}' have been added to the playlist '{new_playlist_name}'.")
+    while True:
+        results = sp.playlist_tracks(playlist_id=source_playlist_id, limit=limit, offset=offset)
+        tracks.extend(results["items"])
+        if len(results["items"]) < limit:
+            # All tracks fetched
+            break
+        offset += limit
+        time.sleep(1)  # Add a 1-second delay between requests to stay within the rate limit
+
+
+    sorted_tracks = sorted(tracks, key=lambda x: x["track"]["duration_ms"])
+    
+    for track in (sorted_tracks):
+        print( track["track"]["name"], track["track"]["duration_ms"])
+
+
+    shortest_tracks = sorted_tracks[:20]
+    track_uris = [track["track"]["uri"] for track in shortest_tracks]
+
+    total_tracks = len(track_uris)
+    batch_size = 100  # Maximum number of tracks per API request to add to the playlist
+
+    
+    for i in range(0, total_tracks, batch_size):
+        sp.playlist_add_items(playlist_id=new_playlist["id"], items=track_uris[i:i+batch_size])
+        time.sleep(1)  # Add a 1-second delay between batch requests to stay within the rate limit
+    
+
+    print(f"The 20 shortest songs have been added to the playlist '{new_playlist_name}'.")
 
 # Example usage
 if __name__ == "__main__":
-    source_playlist_name = "My Shazam Tracks"
-    new_playlist_name = "test1"
+    source_playlist_id = "7eG04lBozqMlzgmpM1omp3"  # Replace with the ID of the source playlist
+    new_playlist_name = "Indie Sounds"
 
-
-    save_shortest_tracks_to_playlist(source_playlist_name, new_playlist_name)
+    save_shortest_songs_to_playlist(source_playlist_id, new_playlist_name)
