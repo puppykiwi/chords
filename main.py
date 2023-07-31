4d12ee2ade26f28c85a51ce033dfd9b7a8918fec
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-31 09:57:14 +0300

    user_playlists

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 
-def get_playlist_id(playlist_name)
+def get_playlist_id(playlist_name):
     scope = "user-library-read playlist-modify-private"
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
@@ -70,9 +70,23 @@ def save_shortest_songs_to_playlist(source_playlist_id, new_playlist_name):
 
     print(f"The 20 shortest songs have been added to the playlist '{new_playlist_name}'.")
 
+def get_user_playlists():
+    scope = "user-library-read playlist-modify-private"
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+
+    current_user_id = sp.current_user()["id"]
+
+    playlists = sp.user_playlists(user=current_user_id)
+    if playlists["items"]:
+        for playlist in playlists["items"]:
+            print("\nPlaylist name: ",playlist["name"],"\nPlaylist ID: ", playlist["id"]) # debug
+            
+            
+
 # Example usage
 if __name__ == "__main__":
     source_playlist_id = "7eG04lBozqMlzgmpM1omp3"  # Replace with the ID of the source playlist
     new_playlist_name = "Indie Sounds"
 
-    save_shortest_songs_to_playlist(source_playlist_id, new_playlist_name)
+    get_user_playlists()
