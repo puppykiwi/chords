154d3d4c9e608ee74910cd7d7e0f1bddbe1789a9
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-31 08:54:08 +0300

    newline

--- analyze.py
+++ analyze.py
@@ -37,7 +37,7 @@ def get_playlist_tracks(playlist_id):
         results = sp.playlist_tracks(playlist_id=playlist_id, limit=limit, offset=offset)
         tracks.extend(results["items"])
         if len(results["items"]) < limit:
-            print("All tracks fetched")
+            print("\nAll tracks fetched")
             break
         offset += limit
         time.sleep(1) 
