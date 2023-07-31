7151ee5fd145eb1990a1815128f30756c76910f6
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-31 09:56:56 +0300

    rank_tracks and pagination

--- analyze.py
+++ analyze.py
@@ -37,7 +37,7 @@ def get_playlist_tracks(playlist_id):
         results = sp.playlist_tracks(playlist_id=playlist_id, limit=limit, offset=offset)
         tracks.extend(results["items"])
         if len(results["items"]) < limit:
-            print("\nAll tracks fetched")
+            #print("\nAll tracks fetched") #debug
             break
         offset += limit
         time.sleep(1) 
@@ -45,19 +45,39 @@ def get_playlist_tracks(playlist_id):
     track_dict = {}
     for index, track_info in enumerate(tracks):
         track_name = track_info["track"]["name"]
+        #print(index, track_name) # debug
         track_uri = track_info["track"]["uri"]
         track_dict[index] = {"name": track_name, "uri": track_uri}
 
     return track_dict
 
+
 def get_audio_features(track_dict):
     track_uris = [track["uri"] for track in track_dict.values()]
-    audio_features = sp.audio_features(track_uris)
-    print(audio_features) # debug
+    audio_features = []
+
+    batch_size = 50
+
+    for i in range(0, len(track_uris), batch_size):
+        batch_track_uris = track_uris[i:i + batch_size]
+        batch_audio_features = sp.audio_features(batch_track_uris)
+
+        for j, track_info in enumerate(batch_audio_features):
+            track_name = track_dict[i + j]["name"]
+            track_info["name"] = track_name
+
+        audio_features.extend(batch_audio_features)
+
     return audio_features
 
+def rank_tracks(audio_features, feature=input("Enter a feature to rank by: ")):
+    ranked_tracks = sorted(audio_features, key=lambda x: x[feature], reverse=True)
+    for index, track in enumerate(ranked_tracks):
+        print((index+1 ), track["name"], track[feature])
+    return ranked_tracks
 
 if __name__ == "__main__":
-    playlist_id = get_playlist_id("Indie Sounds")
+    playlist_id = get_playlist_id("Liked Songs by kiwi")
     tracks = get_playlist_tracks(playlist_id)
     audio_features = get_audio_features(tracks)
+    ranked_tracks = rank_tracks(audio_features)
